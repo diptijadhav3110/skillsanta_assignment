bdb2ec34d402c07d09d83dddff38e19a8496664f
diptijadhav3110 <67471408+diptijadhav3110@users.noreply.github.com>
2021-05-07 22:27:36 +0530

    Add files via upload

---
+++ range.js
@@ -0,0 +1,7 @@
+let num=parseInt(prompt("Enter a num"));
+if (num>=50 && num<=100) {
+    document.write("Given number is in the range");
+    
+} else {
+    document.write("given number is not in the range");
+}

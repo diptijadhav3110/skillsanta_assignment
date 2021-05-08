d17fbc34e7ed5ed581c7798fae784624aa750149
diptijadhav3110 <67471408+diptijadhav3110@users.noreply.github.com>
2021-05-08 21:18:57 +0530

    Add files via upload

---
+++ oddeven.js
@@ -0,0 +1,13 @@
+function oddEven(){
+    for(let i=20; i<=30;i++){
+        if (i % 2 ==0) {
+            document.write(i + " is even<br>");
+            
+        } else {
+            document.write(i + " is odd<br>");
+            
+        }
+    }
+}
+
+oddEven();
